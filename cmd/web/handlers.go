c5dde8341b28e1506ca60dc535d84e59c5f847af
Danny Rottstegge <danny@rottstegge.net>
2025-07-31 14:41:56 +0200

    update html

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,6 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 	}
 
