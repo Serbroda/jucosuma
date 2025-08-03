d006003253e12079fa10d222689b699d9983f9e6
Danny Rottstegge <danny@rottstegge.net>
2025-08-03 22:49:31 +0200

    use truncate for long texts

--- ui/v1/src/pages/HomePage.tsx
+++ ui/v1/src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
-import {Link, useLoaderData} from "react-router";
+import {NavLink, useLoaderData} from "react-router";
 import {Button} from "../components/catalyst/button.tsx";
-import {PlusIcon} from "@heroicons/react/16/solid";
+import {ChevronRightIcon, PlusIcon} from "@heroicons/react/16/solid";
+import {Badge} from "../components/catalyst/badge.tsx";
+import {classNames} from "../utils/dom.utils.ts";
+import image from "../assets/image.png";
+import {Avatar} from "../components/catalyst/avatar.tsx";
 
 export default function HomePage() {
     const {contracts} = useLoaderData();
@@ -11,15 +15,35 @@ export default function HomePage() {
 
             <div className="flex flex-col gap-4">
                 <Button className="self-end hover:cursor-pointer" to="/contracts/add">
-                    <PlusIcon />
+                    <PlusIcon/>
                     Add Contract
                 </Button>
 
-                {contracts?.map((contract: any) =>
-                    <div key={contract.id}>
-                        <Link to={"/contracts/" + contract.id}>{contract.name}</Link>
-                    </div>
-                )}
+                <ul className="flex flex-col">
+                    {contracts?.map((contract: any) =>
+                        <li
+                            key={contract.id}
+                            className="">
+                            <NavLink
+                                to={"/contracts/" + contract.id}
+                                className="p-2 flex gap-2 items-center border-b border-zinc-200 dark:border-zinc-800 hover:cursor-pointer hover:bg-zinc-950/5 dark:hover:bg-white/5">
+
+                                <Avatar
+                                    square={true}
+                                    className={classNames("w-10 h-10", contract?.icon_source ? '' : 'dark:invert')}
+                                    src={contract?.icon_source || image}/>
+
+                                <div className="flex flex-col grow min-w-0">
+                                    <div className="truncate text-base/6 font-semibold text-zinc-950 dark:text-white">{contract.name}</div>
+                                    <div  className="text-sm/6 text-zinc-500 dark:text-zinc-400">{contract.contract_type} - {contract.company}</div>
+                                </div>
+
+                                <Badge className="shrink-0">{contract.costs} €</Badge>
+                                <ChevronRightIcon className={"h-5 w-5 shrink-0 text-zinc-950 dark:text-white"}/>
+                            </NavLink>
+                        </li>
+                    )}
+                </ul>
             </div>
         </>
     )
